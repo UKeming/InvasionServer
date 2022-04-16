9d2af12e6fda595a785a68e74cb64200ec8a85e9
“keming” <ukeming@icloud.com>
2022-04-16 22:46:21 +0800

    Initial Commit.

---
+++ src/main/java/com/keming/invasion/InvasionApplication.java
@@ -0,0 +1,13 @@
+package com.keming.invasion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvasionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvasionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keming/invasion/InvasionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keming.invasion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvasionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
